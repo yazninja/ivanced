f45d3226f4056ad53ac6f6f513e0cbaa60e79bcf
yazninja <yazlesean@gmail.com>
2022-04-06 14:25:02 +0800

    Alpha - navbar force bottom

--- index.frontend.js
+++ index.frontend.js
@@ -57,7 +57,7 @@ class iVancedPlugin {
             document.documentElement.classList.remove("dark");
             document.documentElement.classList.remove("light");
         }
-        app.chrome.forceDirectives["appNavigation"] = { value: "true" }
+        app.chrome.forceDirectives["appNavigation"] = { value: "seperate" }
     }
 }
 

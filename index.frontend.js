8de505dadf97d920d5aef37f96e9033533eaf421
yazninja <yazlesean@gmail.com>
2022-05-17 09:59:30 +0800

    fix functions

--- index.frontend.js
+++ index.frontend.js
@@ -3,49 +3,27 @@ class iVancedPlugin {
         CiderFrontAPI.StyleSheets.Add("./plugins/ivanced/less/cupertinoicns.less")
         CiderFrontAPI.StyleSheets.Add("./plugins/ivanced/less/cupertinofont.less")
         CiderFrontAPI.StyleSheets.Add("./plugins/ivanced/less/appleDrawer.less")
+        
         app.$watch('page', async function (newVal, oldVal) {
             this.theme = await CiderCache.getCache("theme-settings")
-            setTimeout(() => {
-                if (newVal !== oldVal) {
-                    if (this.theme.fullArtworkQual) {
-                        console.log("Full Artwork Quality")
-                        let artworks = document.getElementsByClassName("mediaitem-artwork--img")
-                        for (let artwork of artworks) {
-                            artwork.src = artwork.src.replace("/190x190", "/400x400")
-                        }
-                    }
-                    else {
-                        let artworks = document.getElementsByClassName("mediaitem-artwork--img")
-                        for (let artwork of artworks) {
-                            artwork.src = artwork.src.replace("/400x400", "/190x190")
-                        }
-                    }
-                    console.log("Changing Artwork Quality")
-                }
-                if (newVal === "search") {
-                    let artworks = document.getElementsByClassName("mediaitem-artwork--img")
-                    for (let artwork of artworks) {
-                        artwork.src = artwork.src.replace("cc", "bb")
-                    }
-                    console.log("Changing Genre Artwork Quality")
-                }
-            }, 500)
-            })
-            const menuEntry = new CiderFrontAPI.Objects.MenuEntry()
-            this.menuEntryId = uuidv4()
-            menuEntry.Id = this.menuEntryId
-            menuEntry.name = "iTheme Advanced Settings"
-            menuEntry.onClick = () => {
-                app.appRoute("plugin/ivanced-settings")
-            }
-            CiderFrontAPI.AddMenuEntry(menuEntry)
+            setTimeout(() => { this.ChangeArtworkQuality() }, 100)
+        })
 
+        const menuEntry = new CiderFrontAPI.Objects.MenuEntry()
+        this.menuEntryId = uuidv4()
+        menuEntry.Id = this.menuEntryId
+        menuEntry.name = "iTheme Advanced Settings"
+        menuEntry.onClick = () => {
+            app.appRoute("plugin/ivanced-settings")
         }
+        CiderFrontAPI.AddMenuEntry(menuEntry)
+        this.LoadSettings()
+    }
     async LoadSettings() {
-            this.theme = await CiderCache.getCache("theme-settings")
+        this.theme = await CiderCache.getCache("theme-settings")
         theme = this.theme
         console.log("iVanced Cached Settings" + this.theme)
-        if(this.theme.appleIcons) {
+        if (this.theme.appleIcons) {
             document.getElementById("app").classList.add("cupertino-icns")
         }
         else {
@@ -90,6 +68,33 @@ class iVancedPlugin {
             document.getElementById("app").classList.remove("navbar-sidebar");
             document.getElementById("app").classList.remove("navbar-topbar");
             document.getElementById("app").classList.add("navbar");
+        }
+    }
+    ChangeArtworkQuality() {
+        if (newVal !== oldVal) {
+            if (this.theme.fullArtworkQual) {
+                console.log("Full Artwork Quality")
+                let artworks = document.getElementsByClassName("mediaitem-artwork--img")
+                for (let artwork of artworks) {
+                    artwork.src = artwork.src.replace("/190x190", "/400x400")
+                }
+                setTimeout(() => {
+                    if (newVal === "search") {
+                        let artworks = document.getElementsByClassName("mediaitem-artwork--img")
+                        for (let artwork of artworks) {
+                            artwork.src = artwork.src.replace("cc", "bb")
+                        }
+                        console.log("Changing Genre Artwork Quality")
+                    }
+                }, 900)
+            }
+            else {
+                let artworks = document.getElementsByClassName("mediaitem-artwork--img")
+                for (let artwork of artworks) {
+                    artwork.src = artwork.src.replace("/400x400", "/190x190")
+                }
+            }
+            console.log("Changing Artwork Quality")
         }
     }
 }
